0308c8357b3b5d4134b97eae57628011d4c1ae7f
Mohamed Adel <Mohamedadelmohamed997@gmail.com>
2021-01-13 16:37:58 +0200

    get temperture from the api

--- website/app.js
+++ website/app.js
@@ -3,6 +3,10 @@ const generateButton = document.getElementById('generate');
 const zipcode = document.getElementById('zip');
 const feeling = document.getElementById('feelings');
 
+// Api variables
+const URL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+const KEY = '3de64ad60effb9ba2fb9600ffcd8b833';
+
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
@@ -12,6 +16,19 @@ generateButton.addEventListener('click', () => {
     zipcode: zipcode.value,
     feeling: feeling.value,
   };
+
+  getTemp(URL, data.zipcode, KEY);
 });
 
-function getData(route, data) {}
+const getTemp = async function (URL, zip, key) {
+  const response = await fetch(URL + zip + '&appid=' + key);
+
+  try {
+    // save fetch result to send it into the POST later
+    const temperature = await response.json();
+    console.log(temperature);
+    return temperature;
+  } catch (error) {
+    console.log('error', error);
+  }
+};
