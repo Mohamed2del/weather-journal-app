988e13a01d44900b1dbfb6235f9ac0d9b137429c
Mohamed Adel <Mohamedadelmohamed997@gmail.com>
2021-01-13 14:40:54 +0200

    get data from user into js object

--- website/app.js
+++ website/app.js
@@ -1,5 +1,16 @@
 /* Global Variables */
+const generateButton = document.getElementById('generate');
+const zipcode = document.getElementById('zip');
+const feeling = document.getElementById('feelings');
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+generateButton.addEventListener('click', () => {
+  const data = {
+    zipcode: zipcode.value,
+    feeling: feeling.value,
+  };
+  console.log(data);
+});
