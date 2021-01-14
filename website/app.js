028c6daaa9b5d0856421295d3365a7e68ea3f977
Mohamed Adel <Mohamedadelmohamed997@gmail.com>
2021-01-14 10:18:38 +0200

    Update ui and post request

--- website/app.js
+++ website/app.js
@@ -3,6 +3,7 @@ const BaseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
 
 // small functions to make it readable
 
+const generateButton = document.getElementById('generate');
 // getDate
 function getCurrentDate() {
   let d = new Date();
@@ -37,8 +38,50 @@ const getWeather = async (url = '', key, zipCode = '') => {
   }
 };
 
+// Post Data =>  string url / data object
+const postData = async (url = '', data = {}) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+
+  try {
+    const newData = await response.json();
+    return newData;
+  } catch (error) {
+    console.log('error', error);
+  }
+};
+
+// update ui through DOM
 function updateUI(date, temperature, content) {
   document.getElementById('date').innerText = date;
   document.getElementById('temp').innerText = temperature;
   document.getElementById('content').innerText = content;
 }
+
+generateButton.addEventListener('click', () => {
+  url = BaseURL;
+
+  getWeather(url, Key, getZipCode())
+    .then((weatherData = {}) => {
+      const data = {
+        temperature: weatherData.main.temp,
+        date: getCurrentDate(),
+        user_response: getUserResponse(),
+      };
+      return data;
+    })
+    .then((data = {}) => {
+      console.log(data);
+      postData('/add', data);
+      return data;
+    })
+    .then((data = {}) => {
+      updateUI(data.date, data.temperature, data.user_response);
+    });
+});
